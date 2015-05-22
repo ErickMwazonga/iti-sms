36177870f2612ea5c824b1415425108102845694
shcrossan <shanecrossan@gmail.com>
2015-05-21 15:21:36 -1000

    viewContact to sandbox for dev

--- send_sms/views.py
+++ send_sms/views.py
@@ -159,31 +159,31 @@ def deleteTemplate(request, templateID):
 
 @login_required
 def sandbox(request):
-    form = SendMsgForm(request.POST or None)
-    if form.is_valid():
-        number = form.cleaned_data['phoneNumber']
-        #selected_contacts = request.POST.getlist['contacts']
-        number_list = number.split(", ")
-        #number_list.append(selected_contacts)
-        message = form.cleaned_data['message']
-        deviceID = request.POST.get('deviceID')
-        device_obj = device.objects.all()
-        for d_obj in device_obj:
-            if d_obj.user == request.user:
-                for num in number_list:
-                    accountEmail = d_obj.accountEmail
-                    accountPassword = d_obj.accountPassword
-                    gateway = SmsGateway()
-                    gateway.loginDetails(accountEmail, accountPassword)
-                    gateway.sendMessageToNumber(num, message, deviceID)
-        messages.success(request, 'Message Envoye')
-        return redirect('/messages/0')
-    username = request.user.username
-    device_obj = device.objects.all()
     contact_list = contacts.objects.filter(user=request.user)
-    group_list = contactgroup.objects.filter(contact__user=request.user).distinct()
-    template_list = msgTemplates.objects.filter(user=request.user).distinct()
-    context = {"form": form}
-    template = "sendsms.html"
-    pg = ['active', '', '']
+    form = AddContactForm(request.POST or None)
+    if form.is_valid():
+        save_it = form.save(commit=False)
+        save_it.user = request.user
+        save_it.save()
+        messages.success(request, 'Contact Saved')
+        return HttpResponseRedirect('')
+
+    contact_group = contactgroup.objects.filter(user=request.user)
+    groupForm = AddContactToGroupForm(request.POST or None)
+    if groupForm.is_valid():
+        cg = contactgroup()
+        cg.user = request.user
+        cg.groupName = groupForm.cleaned_data['groupName']
+        checklist = request.POST.getlist('checks[]')
+        cg.save()
+        for num in checklist:
+            c = contacts.objects.filter(user=request.user).get(phoneNumber=num)
+            cg.contact.add(c)
+        return HttpResponseRedirect('')
+    active = int(page)
+    pagesize = 10
+    pages = range(((len(contact_list) - 1) / pagesize) + 1)
+    contacts_pag = contact_list[active * pagesize: (active * pagesize) + pagesize]
+    pg = ['', '', 'active']
+    template = "sandbox.html"
     return render_to_response(template, locals(), context_instance=RequestContext(request))
